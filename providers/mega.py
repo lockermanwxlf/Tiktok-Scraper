7d0adda84134265ff976d72422fba016105e913a
lockermanwxlf <95518889+lockermanwxlf@users.noreply.github.com>
2024-05-29 15:46:18 -0500

    Add filter_list to mega provider

--- providers/mega.py
+++ providers/mega.py
@@ -1,6 +1,8 @@
 import requests
 import os
+from posixpath import join
 from .provider import Provider, Provision
+import re
 
 class MegaProvider(Provider):
     def __init__(self, mega_path) -> None:
@@ -10,9 +12,26 @@ class MegaProvider(Provider):
         return requests.get(
             url="http://localhost:15001/exists",
             params={
-                "path": os.path.join(self.__mega_path, directory, filename)
+                "path": join(self.__mega_path, directory, filename)
             }
             ).json()["result"]
+        
+    def filter_list(self, directory: str, post_ids: list[str]):
+        existing_filenames = requests.get(
+            url="http://localhost:15001/list_dir",
+            params={
+                "path": join(self.__mega_path, directory)
+            }
+        ).json()["result"]
+        pattern = re.compile(r"\d+")
+        matches = [
+            pattern.search(filename) for filename in existing_filenames
+        ]
+        matches = [
+            match.group(0) for match in matches if match
+        ]
+        matches = list(set(matches))
+        return [post_id for post_id in post_ids if post_id not in matches]
     
     def provision(self, directory: str, filename: str) -> Provision:
         os.makedirs("tmp", exist_ok=True)
@@ -21,7 +40,7 @@ class MegaProvider(Provider):
             requests.put(
                 url="http://localhost:15001/upload",
                 data={
-                    "path": os.path.join(self.__mega_path, directory, filename)
+                    "path": join(self.__mega_path, directory, filename)
                 },
                 files={
                     "file": open(f"tmp/{filename}", "rb")
